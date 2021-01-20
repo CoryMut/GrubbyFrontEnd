47f19f8a95894be9c4958aeddaedf99384c1299a
CoryMut <59666828+CoryMut@users.noreply.github.com>
2021-01-20 13:21:23 -0500

    blank white screen fix

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, "./build")));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./build", "index.html"));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(8080, () => console.log("Express server is running on port 8080"));
